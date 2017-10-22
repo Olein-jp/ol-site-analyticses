8a2d2a15270950ac1997f394cfa1c3b0b9d04644
KojiKuno <koji.kuno@gmail.com>
2017-10-22 14:26:33 +0900

    initial commit

---
+++ ol-site-analyticses.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name:     Ol Site Analyticses
+ * Plugin URI:      https://github.com/***
+ * Description:     This plugin will be able to use Google Analytics and Search console for your WordPress site.
+ * Author:          Koji Kuno
+ * Author URI:      https://olein-design.com
+ * Text Domain:     ol-site-analyticses
+ * Domain Path:     /languages
+ * Version:         1.0.0
+ *
+ * @package         Ol_Site_Analyticses
+ */
+
+ /*
+	Copyright (c) 2017 Koji KUno (https://olein-design.com/)
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as
+	published by the Free Software Foundation.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
